d050a8ef8e332b24bed2f3e96c1a43321ec95281
gaoyuyu <740514999@qq.com>
2019-05-03 21:41:51 +0800

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -35,8 +36,22 @@ class _HomePageState extends State<HomePage> {
               if (snapshot.hasData) {
                 var data = json.decode(snapshot.data.toString());
                 List<Map> swiper = (data["data"]["slides"] as List).cast();
+                List<Map> navigator = (data["data"]["category"] as List).cast();
+                String adPicture =
+                    data["data"]["advertesPicture"]["PICTURE_ADDRESS"];
+                String leaderImage =
+                    data['data']['shopInfo']['leaderImage']; //店长图片
+                String leaderPhone =
+                    data['data']['shopInfo']['leaderPhone']; //店长电话
+
                 return Column(
-                  children: <Widget>[SwiperDiy(swiperDataList: swiper)],
+                  children: <Widget>[
+                    SwiperDiy(swiperDataList: swiper),
+                    TopNavigator(navgiatorList: navigator),
+                    AdBanner(adPicture: adPicture),
+                    LeaderPhone(
+                        leaderImage: leaderImage, leaderPhone: leaderPhone)
+                  ],
                 );
               } else {
                 return Center(child: Text("加载中"));
@@ -53,19 +68,104 @@ class SwiperDiy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
-
     return Container(
       width: ScreenUtil().setWidth(750),
       height: ScreenUtil().setHeight(333),
       child: Swiper(
         itemBuilder: (BuildContext context, int index) {
-          return Image.network("${swiperDataList[index]['image']}",fit: BoxFit.fill,);
+          return Image.network(
+            "${swiperDataList[index]['image']}",
+            fit: BoxFit.fill,
+          );
         },
         itemCount: swiperDataList.length,
         pagination: SwiperPagination(),
         autoplay: true,
       ),
     );
+  }
+}
+
+//网格导航
+class TopNavigator extends StatelessWidget {
+  final List navgiatorList;
+
+  TopNavigator({this.navgiatorList});
+
+  Widget _gridViewItemUI(BuildContext context, item) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: <Widget>[
+          Image.network(item["image"], width: ScreenUtil().setWidth(95)),
+          Text(item["mallCategoryName"])
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (this.navgiatorList.length > 10) {
+      this.navgiatorList.removeRange(10, 11);
+    }
+
+    return Container(
+      margin: EdgeInsets.only(top: 5.0),
+      height: ScreenUtil().setHeight(320),
+      padding:EdgeInsets.all(3.0),
+      child: GridView.count(
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(4.0),
+        children: navgiatorList.map((item) {
+          return _gridViewItemUI(context, item);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+//广告区域
+class AdBanner extends StatelessWidget {
+  final String adPicture;
+
+  AdBanner({this.adPicture});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.network(adPicture),
+    );
+  }
+}
+
+class LeaderPhone extends StatelessWidget {
+  //店长图片
+  final String leaderImage;
+
+  //店长电话
+  final String leaderPhone;
+
+  LeaderPhone({this.leaderImage, this.leaderPhone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: InkWell(
+        onTap: _launchURL,
+        child: Image.network(leaderImage),
+      ),
+    );
+  }
+
+  void _launchURL() async {
+    String url = "tel:" + leaderPhone;
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+    else
+      {
+        throw "Url不能进行访问，异常";
+      }
   }
 }
