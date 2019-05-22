17eff294626524e67d223cc27f8f2cdfa6a2d345
gaoyuyu <740514999@qq.com>
2019-05-22 11:32:13 +0800

    update

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provide/provide.dart';
 import '../provide/details_info.dart';
+import './details_page/details_top_area.dart';
+import './details_page/details_explain.dart';
 
 
 class DetailsPage extends StatelessWidget {
@@ -24,8 +26,8 @@ class DetailsPage extends StatelessWidget {
               return Container(
                 child: Column(
                   children: <Widget>[
-                    Text(goodsId)
-
+                    DetailsTopArea(),
+                    DetailsExplain()
                   ],
                 ),
               );
--- lib/provide/details_info.dart
+++ lib/provide/details_info.dart
@@ -3,21 +3,21 @@ import '../model/details.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 
-
-class DetailsInfoProvide with ChangeNotifier
-{
+class DetailsInfoProvide with ChangeNotifier {
   DetailsModel goodsInfo = null;
+  bool isLeft = true;
+  bool isRight = false;
+
   //从后台获取商品数据
-  getGoodsInfo(String id)
-  {
-    var formData = {"goodId":id};
-    request("getGoodDetailById",formData: formData).then((val){
+  getGoodsInfo(String id) async {
+    var formData = {
+      'goodId': id,
+    };
+
+    await request('getGoodDetailById', formData: formData).then((val) {
       var responseData = json.decode(val.toString());
-      print(responseData);
       goodsInfo = DetailsModel.fromJson(responseData);
       notifyListeners();
     });
-
-
   }
 }
